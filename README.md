de82428a3a5e8b16d3b08808883dc44149e7928f
Clarke Retzer <clarke@carfoo.com>
2020-06-10 20:47:06 -0500

    Adding the email alert functionality

--- README.md
+++ README.md
@@ -10,18 +10,42 @@ Example configuration:
 
 ```
 ---
+email:
+    port: 587
+    smtp_server: smtp.gmail.com
+    sender_email: user-account@gmail.com
+    password: application-specific-password
 sites:
     google:
       email_recipients:
       - user@example.com
       timeout: 0.1
       url: https://google.com
+      enabled: 1
     yahoo:
       email_recipients:
       - foo@bar.com
       timeout: 1
       url: https://yahoo.com
+      enabled: 0
 ```
+
+## Email
+
+The email section is for the smtp configuration, so that email can be sent.
+
+## Sites
+
+The sites section is for the sites to be checked. Each site should contain:
+
+- url (The URL to be called for the check)
+- timeout (The connect timeout in seconds)
+- email_recipients (A list of email addresses to notify on failure)
+- enabled (Whether to enable the specific site, or to skip it. 0: disabled, 1: enabled)
+
+## Note
+
+Since this configuration file contains an smtp password, the file permissions should be as restrictive as possible. It should ideally be set to 0600, so that only the owner can read and write it.
 
 # Setup
 
--- pinger.py
+++ pinger.py
@@ -5,10 +5,16 @@ import requests
 import yaml
 from pathlib import Path
 import os
+import smtplib, ssl
+import datetime
 # }}}
 
 # Configuration {{{
 configuration_file = '~/.pinger.conf'
+email_port = None
+email_smtp_server = None
+email_sender_email = None
+email_sender_password = None
 # }}}
 
 # Custom Exceptions {{{
@@ -20,6 +26,18 @@ def get_configuration():
     with open(Path(os.path.expanduser('~/.pinger.conf'))) as f:
         conf = yaml.load(f, Loader=yaml.FullLoader)
     return conf
+
+def setup_email_configuration(email_config):
+    global email_port
+    global email_smtp_server
+    global email_sender_email
+    global email_sender_password
+
+    email_port            = email_config['port']
+    email_smtp_server     = email_config['smtp_server']
+    email_sender_email    = email_config['sender_email']
+    email_sender_password = email_config['password']
+
 
 def check_site(site):
     try:
@@ -34,14 +52,36 @@ def check_site(site):
         print(f"Exception occurred: {e}")
         return False
 
+def send_email(message, recipient):
+	context = ssl.create_default_context()
+	with smtplib.SMTP(email_smtp_server, email_port) as server:
+		server.starttls(context=context)
+		server.login(email_sender_email, email_sender_password)
+		server.sendmail(email_sender_email, recipient, message)
+
 if __name__ == "__main__": # {{{
     configuration = get_configuration()
 
+    setup_email_configuration(configuration['email'])
+
     for site in configuration['sites'].keys():
-        if not check_site(configuration['sites'][site]):
-            print(f"Check failed for {site}")
-        else:
-            print(f"Successful check for {site}")
+        site_config = configuration['sites'][site]
+        if site_config['enabled'] == 1:
+            if not check_site(site_config):
+                msg = f"""Subject: Site check failed for {site}
+
+    Failed site check for {site}
+
+    ---
+    Timestamp: {datetime.datetime.now().isoformat()}
+    URL: {site_config['url']}
+    Timeout: {site_config['timeout']}
+                """
+                for email in site_config['email_recipients']:
+                    send_email(msg, email)
+            else:
+                # print(f"Successful check for {site}")
+                pass
 # }}}
 
 # vim: foldmethod=marker foldlevel=0
