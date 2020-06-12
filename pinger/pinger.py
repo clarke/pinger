dce10a66113a0902ed7fce074ab2f65fc5e8d312
Clarke Retzer <clarke@carfoo.com>
2020-06-11 20:32:55 -0500

    Removing print statements

--- pinger/pinger.py
+++ pinger/pinger.py
@@ -6,13 +6,11 @@ def check_site(site):
     try:
         res = requests.get(site['url'], timeout=site['timeout'])
         if res.status_code != 200:
-            print(f"Status code: {res.status_code}")
             return False
         return True
     except requests.exceptions.ConnectTimeout as cte:
         return False
     except Exception as e:
-        print(f"Exception occurred: {e}")
         return False
 
 # vim: foldmethod=marker foldlevel=0
